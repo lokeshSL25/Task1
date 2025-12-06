22c7e3be0e23c9cbd965f1fa6a1ee8c8a26babdf
Student <Student@DK-37>
2025-12-06 11:50:47 +0530

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class TransactionAnalyzer
+{
+    static void Main()
+    {
+        string transactionId = "TRX123AB89YZ"; // 12-character alphanumeric
+        decimal amount = 1000000.75m; // financial amount
+        bool isInternational = true; // domestic/international
+        decimal customerRating = 4.75m; // rating (2 decimal)
+        DateTime transactionTimestamp = DateTime.Now; // date & time
+        int rewardPoints = 1250000; // whole number
+
+        Console.WriteLine("Transaction Analyzer Running...");
+        Console.WriteLine($"ID: {transactionId}");
+        Console.WriteLine($"Amount: ₹{amount}");
+        Console.WriteLine($"International: {isInternational}");
+        Console.WriteLine($"Rating: {customerRating}");
+        Console.WriteLine($"Timestamp: {transactionTimestamp}");
+        Console.WriteLine($"Reward Points: {rewardPoints}");
+    }
+}
